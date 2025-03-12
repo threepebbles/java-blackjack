c8534926261f2e9f5a2121c0491570f848d9f863
threepebbles <threepebbles@naver.com>
2025-03-12 09:37:13 +0900

    refactor: 딜러명 상수화

--- src/main/java/domain/player/Dealer.java
+++ src/main/java/domain/player/Dealer.java
@@ -4,9 +4,10 @@ import domain.card.Deck;
 
 public class Dealer extends Player {
     private static final int ADD_CARD_THRESHOLD = 16;
+    private static final String DEALER_NAME = "딜러";
 
     public Dealer() {
-        super("딜러");
+        super(DEALER_NAME);
     }
 
     @Override
